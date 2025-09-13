9e5848c21eff866e9cb5bfe2e6fcd85180b43c0c
HimanshuXDebug <hjjoshi020@gmail.com>
2025-09-13 21:07:02 +0530

    feat: added new components  to main App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import Header from "./components/Header";
 import { PipelineUI } from "./ui";
 import { handleSubmit } from "./submit";
 import { RunPanel } from "./components/RunPanel";
-// import Sidebar from './components/Sidebar';
-
+import {DeviceAndLinkCheck} from "./Validation/DeviceCheck";
+import { Toaster } from "sonner";
 function App() {
   const [popup, setPopup] = useState({ visible: false, content: "" });
 
@@ -18,9 +18,10 @@ function App() {
   };
 
   return (
+    <DeviceAndLinkCheck>
     <div className="h-screen flex flex-col from:bg-gray-50 to:bg-blue-600 ">
       <Header onReset={onReset} onSubmit={onSubmit} />
-
+       <Toaster position="top-right" />
       <div className="flex flex-1 overflow-hidden">
         {/* <Sidebar /> */}
         <main className="flex-grow p-4 overflow-auto">
@@ -28,21 +29,8 @@ function App() {
         </main>
         <RunPanel />
       </div>
-
-      {popup.visible && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-          <div className="bg-white p-6 rounded-lg shadow-xl max-w-md w-full text-gray-800 relative">
-            <button
-              onClick={() => setPopup({ visible: false, content: "" })}
-              className="absolute top-2 right-3 text-gray-400 hover:text-gray-600 text-xl"
-            >
-              &times;
-            </button>
-            <pre className="whitespace-pre-wrap text-sm">{popup.content}</pre>
-          </div>
-        </div>
-      )}
     </div>
+    </DeviceAndLinkCheck>
   );
 }
 
